ed9e43d32a58ca45c5858155240d257a454ee03f
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-20 14:54:21 -0300

    Adicionada a rota com id, futuramente o id do tópico

--- portal-es/src/app/camadas/camada-final/camada-final-routing.module.ts
+++ portal-es/src/app/camadas/camada-final/camada-final-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CamadaFinalComponent } from './camada-final/camada-final.component';
+import { TopicoComponent } from './topico/topico.component';
 
 const routes: Routes = [
   {
     path: 'es/detalhe',
-    component: CamadaFinalComponent
+    component: CamadaFinalComponent,
+    children: [
+      {
+        path: 'topico/:id',
+        component: TopicoComponent
+      }
+    ]
   }
 ];
 

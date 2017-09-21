d0f743a047d3d3ff100d7827a083798655222e0f
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-21 18:43:49 -0300

    Criando Metodos para pegar os textos (primarios e secundarios)

--- portal-es/src/app/camadas/camada-final/topico/topico.component.ts
+++ portal-es/src/app/camadas/camada-final/topico/topico.component.ts
@@ -2,19 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
+import { CamadaFinalService } from './../shared/camada-final.service';
+
 @Component({
   selector: 'ufg-es-topico',
   templateUrl: './topico.component.html',
   styleUrls: ['./topico.component.scss']
 })
 export class TopicoComponent implements OnInit {
+  titulo: any;
+  chavesSecundarias: any;
 
   id: string;
   inscricao: Subscription;
 
-  constructor(private route: ActivatedRoute) {
+  ppc: any;
+  meuTexto: any;
+
+  constructor(private route: ActivatedRoute, private finalService: CamadaFinalService) {
     // this.id = this.route.snapshot.params['id'];
     // console.log(this.route);
+    // this.id = '0';
   }
 
   ngOnInit() {
@@ -22,11 +30,39 @@ export class TopicoComponent implements OnInit {
       (params: any) => {
         this.id = params['id'];
         // console.log(this.id);
+        this.getTextoPrimario();
     });
   }
 
   ngOnDestroy() {
     this.inscricao.unsubscribe();
+  }
+
+  getTextoPrimario() {
+    this.finalService.estadoDoPpc.subscribe(
+      data => {
+        this.ppc = data.ppc;
+
+        const myArray = this.ppc.filter(
+          item => {
+            return (item.id === Number(this.id));
+          }
+        );
+
+        // console.log(myArray);
+
+        this.titulo = myArray[0].chave_primaria;
+        this.meuTexto = myArray[0].texto;
+        // console.log(this.meuTexto);
+
+        this.getTextoSecundario(myArray);
+      }
+    );
+  }
+
+  getTextoSecundario(myArray) {
+    this.chavesSecundarias = myArray[0].chave_secundaria;
+    // console.log(this.chavesSecundarias);
   }
 
 }

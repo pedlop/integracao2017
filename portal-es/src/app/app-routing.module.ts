a139de90b48d0605a638cbb74c844606b50c1dac
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-23 22:50:44 -0300

    Testando a pesquisa

--- portal-es/src/app/app-routing.module.ts
+++ portal-es/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CamadaInicialComponent } from './camadas/camada-inicial/camada-inicial/camada-inicial.component';
 import { PaginaNaoEncontradaComponent } from './compartilhado/pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { MenuPesquisaComponent } from './compartilhado/menu-pesquisa/menu-pesquisa.component';
 
 const routes: Routes = [
     {
         path: '',
         component: CamadaInicialComponent
+    },
+    {
+        path: 'teste',
+        component: MenuPesquisaComponent
     },
     {
         path: '**',

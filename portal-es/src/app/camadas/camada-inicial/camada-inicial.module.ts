83f2717a3dd6e004dee74111a44182581538dc37
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-23 23:00:43 -0300

    Organizando template da cama inicial, dividindo e, outros componentes

--- portal-es/src/app/camadas/camada-inicial/camada-inicial.module.ts
+++ portal-es/src/app/camadas/camada-inicial/camada-inicial.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CamadaInicialComponent } from './camada-inicial/camada-inicial.component';
 import { CamadaInicialRoutingModule } from './camada-inicial-routing.module';
 import { CamadaInicialService } from './shared/camada-inicial.service';
+import { MenuPesquisaModule } from './../../compartilhado/menu-pesquisa/menu-pesquisa.module';
 
 @NgModule({
   declarations: [CamadaInicialComponent],  
   imports: [
     CommonModule,
-    CamadaInicialRoutingModule
+    CamadaInicialRoutingModule,
+    MenuPesquisaModule
   ],
   exports: [CamadaInicialComponent],
   providers: [CamadaInicialService]

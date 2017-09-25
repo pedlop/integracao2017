297d597330138c8f99ebf8ebf6c92ea733de48bd
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-25 11:45:31 -0300

    Importando modulo para graficos

--- portal-es/src/app/camadas/camada-medial/camada-medial.module.ts
+++ portal-es/src/app/camadas/camada-medial/camada-medial.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {NgxChartsModule} from '@swimlane/ngx-charts';
+
 import { CamadaMedialComponent } from './camada-medial/camada-medial.component';
 import { CamadaMedialRoutingModule } from './camada-medial-routing.module';
 import { CamadaMedialService } from './shared/camada-medial.service';
 
 @NgModule({
-  declarations: [CamadaMedialComponent],  
+  declarations: [CamadaMedialComponent],
   imports: [
     CommonModule,
+    NgxChartsModule,
     CamadaMedialRoutingModule
   ],
   exports: [CamadaMedialComponent],

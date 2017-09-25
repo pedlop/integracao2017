dd3cbf5f89321c811933ef849879c73efb57e27c
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-09-25 11:45:10 -0300

    Testando grafico na camada medial

--- portal-es/src/app/camadas/camada-medial/camada-medial/camada-medial.component.ts
+++ portal-es/src/app/camadas/camada-medial/camada-medial/camada-medial.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { single, multi } from './../shared/camada-medial';
+
 @Component({
   selector: 'ufg-es-camada-medial',
   templateUrl: './camada-medial.component.html',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CamadaMedialComponent implements OnInit {
 
-  constructor() { }
+  data: any;
+
+  single: any[];
+  multi: any[];
+
+  view: any[] = [700, 400];
+
+  // options
+  showLegend = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  // pie
+  showLabels = true;
+  explodeSlices = false;
+  doughnut = false;
+
+  constructor() {
+    window.scrollTo(0, 0);
+    Object.assign(this, {single, multi});
+    this.data = [
+      {
+      'nome': 'Matemática',
+      'info': [
+        {
+          'ano': '2010',
+          'valor': 799
+        },
+        {
+          'ano': '2011',
+          'valor': 788
+        }
+      ]
+    },
+    {
+      'nome': 'Computaçao',
+      'info': [
+        {
+          'ano': '2010',
+          'valor': 907
+        },
+        {
+          'ano': '2011',
+          'valor': 534
+        }
+      ]
+    }
+  ];
+  }
 
   ngOnInit() {
+
+
+  }
+
+  onSelect(event) {
+    console.log(event);
   }
 
 }

e980846402248bfc83477f2ef4faf042100521f2
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-11-13 17:41:25 -0200

    New api url

--- portal-es/src/environments/environment.prod.ts
+++ portal-es/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api: {
-    url: 'https://cdn.rawgit.com/pedlop/integracao2017/5fa3086c/portal-es/src/assets/led/ppc-es.json'
+    url: 'https://cdn.rawgit.com/pedlop/integracao2017/78b42e74/portal-es/src/assets/led/ppc-es.json'
   }
 };

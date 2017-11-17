bd02aa96d5801ac8923a5e60f7250be351b67e63
Pedro Victor <pedrolopes09.pv@gmail.com>
2017-11-17 11:55:16 -0200

    Update api url e dependência do side menu

--- portal-es/src/environments/environment.prod.ts
+++ portal-es/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api: {
-    url: 'https://cdn.rawgit.com/pedlop/integracao2017/2fb38584/portal-es/src/assets/led/ppc-es.json'
+    url: 'https://cdn.rawgit.com/pedlop/integracao2017/9c777604/portal-es/src/assets/led/ppc-es.json'
   },
   camadaMedialUrl : '/integracao2017/es/disciplinas'
 };

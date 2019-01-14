2c8d8809135e6d2503f55f8176a43381a2b41bfa
Pedro Lopes <pedrolopes09.pv@gmail.com>
2019-01-14 09:45:29 -0200

    Update environment.prod.ts

--- portal-es/src/environments/environment.prod.ts
+++ portal-es/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api: {
-    url: 'https://cdn.rawgit.com/pedlop/integracao2017/b496ac65/portal-es/src/assets/led/ppc-es.json'
+    url: 'https://cdn.jsdelivr.net/gh/pedlop/integracao2017@b496ac65478fe76d5ff7343a11bbcb5ae5634a3f/portal-es/src/assets/led/ppc-es.json'
   },
   camadaMedialUrl : '/integracao2017/es/disciplinas'
 };
